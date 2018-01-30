f48a03f2dd536fecb99fc88fb3066cbe64d07825
surfertas <tasuku@gmail.com>
2018-01-30 21:59:18 +0900

    updated

---
+++ projects/self_driving_car/2-pilot_net/data_loader.py
@@ -0,0 +1,140 @@
+# @author Tasuku Miura
+# @brief PyTorch implementation of PilotNet (Assumes CUDA enabled)
+
+import os
+import pickle
+
+from skimage import io, transform
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# used for logging to TensorBoard
+from tensorboard_logger import configure, log_value
+
+from pilot_net import *
+
+import math
+
+
+
+class DriveDataset(Dataset):
+
+    """
+
+    Custom dataset to handle Udacity drive data.
+
+    """
+
+    def __init__(self, csv_file, root_dir, bags, transform=None):
+        self._csv_file = csv_file
+        self._root_dir = root_dir
+        self._bags = bags
+        self._transform = transform
+        self._frames = self._get_frames()
+
+    def __len__(self):
+        return len(self._frames)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self._root_dir,
+                                self._frames['bag'].iloc[idx],
+                                self._frames['filename'].iloc[idx])
+        image = io.imread(img_name)
+        target = self._frames['angle'].iloc[idx]
+        sample = {'image': image, 'image_path': img_name, 'steer': target}
+
+        if self._transform:
+            sample['image'] = self._transform(sample['image'])
+
+        return sample
+
+    def _get_frames(self):
+        file_paths = [
+            (bag, os.path.join(self._root_dir, bag, self._csv_file))
+            for bag in self._bags
+        ]
+
+        frames = []
+        for bag, path in file_paths:
+            df = pd.read_csv(path)
+            df['bag'] = bag
+            frames.append(df[df.frame_id == 'center_camera'])
+        return pd.concat(frames, axis=0)
+
+
+
+class SequenceDriveDataset(Dataset):
+    """
+
+    Custom dataset to convert Udacity drive data to inputs of sequences.
+
+    """
+
+    def __init__(self, csv_file, root_dir, bags, time_steps, transform=None):
+        self._csv_file = csv_file
+        self._root_dir = root_dir
+        self._bags = bags
+        self._steps = time_steps + 1 # +1 to adjust for indexing
+        self._transform = transform
+        self._frames = self.get_frames()
+
+    def __len__(self):
+        return len(self._frames)
+
+    def __getitem__(self, idx):
+        images = []
+        for step in range(1,self._steps):
+            img_name = os.path.join(
+                self._root_dir,
+                self._frames['filename_L{}'.format(step)].iloc[idx]
+            )
+            images.append(io.imread(img_name))
+        target = self._frames['angle'].iloc[idx]
+        sample = {'image': images, 'steer': target}
+
+        if self._transform:
+            transformed = []
+            for image in sample['image']:
+                transformed.append(self._transform(image))
+            sample['image'] = transformed
+
+        return sample
+
+    def get_frames(self):
+        file_paths = [
+            (bag,os.path.join(self._root_dir, bag, self._csv_file))
+            for bag in self._bags
+        ]
+        frames = []
+        unused = ['width', 'height','lat','long','alt','timestamp']
+        for bag,path in file_paths:
+            df = pd.read_csv(path)
+            # Add bag column and generate correct paths.
+            df['bag'] = bag
+            df.filename = df.bag + '/'+ df.filename
+            # Remove unused to columns.
+            for col in unused:
+                del df[col]
+                
+            frames.append(df[df.frame_id=='center_camera'])
+            
+        all_df = pd.concat(frames, axis=0, ignore_index=True)
+        
+        lags_df = (pd.concat(
+            [all_df.filename.shift(i) for i in range(self._steps)], 
+            axis=1,
+            keys=['filename'] + ['filename_L%s' % i for i in range(1, self._steps)]
+            ).dropna()
+        )
+        # To ensure that only past frames are used to predict current steering.
+        # (e.g to predict target at t, we use t-1,t-2, etc.)
+        lags_df = lags_df.drop(['filename'], axis=1)
+        final_df = pd.merge(all_df, lags_df, left_index=True, right_index=True)
+        return final_df
+  
--- projects/self_driving_car/2-pilot_net/pilot_net.py
+++ projects/self_driving_car/2-pilot_net/pilot_net.py
@@ -33,3 +33,45 @@ class PilotNet(nn.Module):
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+
+
+class PilotNetBn(nn.Module):
+
+    def __init__(self):
+        super(PilotNetBn, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=2, padding=0)
+        self.bn1 = nn.BatchNorm2d(24)
+        self.conv2 = nn.Conv2d(24, 36, kernel_size=5, stride=2, padding=0)
+        self.bn2 = nn.BatchNorm2d(36)
+        self.conv3 = nn.Conv2d(36, 48, kernel_size=5, stride=2, padding=0)
+        self.bn3 = nn.BatchNorm2d(48)
+        self.conv4 = nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=0)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(64 * 1 * 18, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.fc3 = nn.Linear(50, 10)
+        self.fc4 = nn.Linear(10, 1)
+
+    def forward(self, x):
+        x = (x - 0.5) * 2.0
+        x = self.bn1(F.relu(self.conv1(x)))
+        x = self.bn2(F.relu(self.conv2(x)))
+        x = self.bn3(F.relu(self.conv3(x)))
+        x = self.bn4(F.relu(self.conv4(x)))
+        x = self.bn5(F.relu(self.conv5(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+#class PilotNetLSTM(object):
+#    def __init__(self):
+#        super(PilotNetLSTM, self).__init__()
+#        self.rnn = nn.LSTM(
+        
+#batch_first – If True, then the input and output tensors are provided as (batch, seq, feature))
+
