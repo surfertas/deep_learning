f17e009e61df9fbb0e08fbcc661d9401e616f554
surfertas <tasuku@gmail.com>
2019-03-11 22:39:05 +0900

    alexnet as a feature extractor

--- projects/amr_py_controller/model/net.py
+++ projects/amr_py_controller/model/net.py
@@ -6,6 +6,52 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+from torchvision import models
+
+
+class AlexNetConv4(nn.Module):
+
+    """
+    Use conv layers as feature extractor.
+    """
+
+    def __init__(self):
+        super(AlexNetConv4, self).__init__()
+        self.alexnet = models.alexnet(pretrained=True)
+
+        self.features = nn.Sequential(
+            # stop at conv4
+            *list(self.alexnet.features.children())[:-3]
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
+
+
+class PilotNetAlexNetTransfer(nn.Module):
+
+    """
+    Fine tuning with AlexNet. Remove classification layers (basically all the fc
+    layers) and retrain for regression.
+    """
+
+    def __init__(self):
+        super(PilotNetAlexNetTransfer, self).__init__()
+        self.features = AlexNetConv4()
+        self.fc1 = nn.Linear(43264, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+        self.fc3 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 class Net(nn.Module):
     """
     """
