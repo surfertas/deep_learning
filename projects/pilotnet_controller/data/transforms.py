3f8c9b39fa2ab7e864126531912c5e8b8a5f0640
surfertas <tasuku@gmail.com>
2019-04-20 09:03:23 +0000

    adding color jitter

--- projects/pilotnet_controller/data/transforms.py
+++ projects/pilotnet_controller/data/transforms.py
@@ -17,6 +17,7 @@ from torchvision import transforms, utils
 def basenet_transforms(cfg):
     train_transformer = transforms.Compose([
         transforms.Resize((cfg.IMAGE.TARGET_HEIGHT,cfg.IMAGE.TARGET_WIDTH)),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
         transforms.ToTensor()])  # transform it into a torch tensor
 
     # loader for evaluation, keep separate as transformer for train can be different
