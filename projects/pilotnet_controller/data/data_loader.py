efe94559aec407dfbbd90b65e3da278f8ba829e8
surfertas <tasuku@gmail.com>
2019-05-04 09:09:07 +0000

    updated

--- projects/pilotnet_controller/data/data_loader.py
+++ projects/pilotnet_controller/data/data_loader.py
@@ -5,19 +5,20 @@ import os
 
 import torch
 import torchvision.transforms as transforms
+from torchvision.utils import save_image
 from torch.utils.data import Dataset, DataLoader, ConcatDataset
 
 from io import BytesIO
 from PIL import Image
 from google.cloud import storage
 
-from transforms import basenet_transforms 
+from .transforms import basenet_transforms 
 
 
 
 class ControllerDataset(Dataset):
     
-    def __init__(self, cfg, bucket_name, split, datasets, transform, remove_center=False, throttle_include=False):
+    def __init__(self, cfg, bucket_name, split, datasets, transform, throttle_include=False):
         self.cfg = cfg
         self.bucket_name = bucket_name
         self.data_csv = datasets[split]
@@ -27,24 +28,18 @@ class ControllerDataset(Dataset):
         self.n_samples = len(self.target)
         self.augment = self.cfg.IMAGE.DO_AUGMENTATION
         self.transform = transform
-        self.remove_center=remove_center
         self.throttle_include = throttle_include
+
+        self.debug = cfg.MODEL.DEBUG
+        self.cwd = os.path.dirname(os.path.abspath(__file__))
+
 
     def __len__(self):
         return len(self.features)
         
     def __getitem__(self, idx):
         # Target is a array consisting of [throttle, steer]
-        #target = torch.FloatTensor(self.target.iloc[idx].values.tolist())
-        if self.remove_center:
-            while True:
-                target = self.target.iloc[idx].values.tolist()
-                if abs(target[1]) > 0.3:
-                    break
-                else:
-                    idx = int((idx + np.random.randint(self.n_samples, size=1))%self.n_samples)
-        else:
-            target = self.target.iloc[idx].values.tolist()
+        target = self.target.iloc[idx].values.tolist()
 
         image = self._get_image(self.features.iloc[idx])
         
@@ -60,18 +55,22 @@ class ControllerDataset(Dataset):
                 target[1] = np.clip(target[1],-1.0,1.0)
        
        
+        # If DO_AUGMENTATION is true, jitter will be applied to images, else just a resize
+        # will be applied.
         image = self.transform(Image.fromarray(image))
+
+        if self.debug:
+            save_image(image, os.path.join(self.cwd, "train-images/image+{}.png".format(idx)))
     
         # Train on both throttle, steer or just steer
         target = target if self.throttle_include else np.array([target[1]])
-
         target = torch.FloatTensor(target)
     
         return image, target
 
     def _get_image(self, gs_path):
         image = np.array(Image.open(gs_path))
-        #image = self._preprocess(image)
+        image = self._preprocess(image)
         return image
 
     def _preprocess(self, image):
@@ -109,6 +108,7 @@ def fetch_dataloader(types, bucket_name, data_dir, csv_filename, cfg):
     print("size of data train set: {}".format(len(data_train_new)))
     print("size of data val set: {}".format(len(data_eval)))
     print("size of data test set: {}".format(len(data_test)))
+
     datasets = {
         'train': data_train_new,
         'val': data_eval,
@@ -122,15 +122,7 @@ def fetch_dataloader(types, bucket_name, data_dir, csv_filename, cfg):
         if split in types:
             # use the train_transformer if training data, else use eval_transformer without random flip
             if split == 'train':
-                # NOTE: set shuffle to false as not sure what implications are for time series.
-                data_all = ControllerDataset(cfg, bucket_name, split, datasets, train_transformer)
-
-                # Dataset with samples with abs(steer) > 0.6, turns to help balance data.
-                data_no_center = ControllerDataset(cfg, bucket_name, split, datasets, train_transformer, remove_center=True)
-
-                data_concat = ConcatDataset([data_all,data_no_center])
-
-                dl = DataLoader(data_concat,
+                dl = DataLoader(ControllerDataset(cfg, bucket_name, split, datasets, train_transformer),
                                 batch_size=cfg.INPUT.BATCH_SIZE,
                                 shuffle=cfg.DATASETS.SHUFFLE,
                                 num_workers=cfg.DATALOADER.NUM_WORKERS,
