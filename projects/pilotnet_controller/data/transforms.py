990213fe73df97a0e273fe1e9e3bada957cb2e0f
surfertas <tasuku@gmail.com>
2019-05-16 23:57:24 +0900

    pep

--- projects/pilotnet_controller/data/transforms.py
+++ projects/pilotnet_controller/data/transforms.py
@@ -14,26 +14,25 @@ from torch.autograd import Variable
 
 from torchvision import transforms, utils
 
+
 def basenet_transforms(cfg):
 
     if cfg.IMAGE.DO_AUGMENTATION:
         train_transformer = transforms.Compose([
-            transforms.Resize((cfg.IMAGE.TARGET_HEIGHT,cfg.IMAGE.TARGET_WIDTH)),
+            transforms.Resize((cfg.IMAGE.TARGET_HEIGHT, cfg.IMAGE.TARGET_WIDTH)),
             transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
             transforms.ToTensor()])  # transform it into a torch tensor
     else:
         train_transformer = transforms.Compose([
-            transforms.Resize((cfg.IMAGE.TARGET_HEIGHT,cfg.IMAGE.TARGET_WIDTH)),
+            transforms.Resize((cfg.IMAGE.TARGET_HEIGHT, cfg.IMAGE.TARGET_WIDTH)),
             transforms.ToTensor()])  # transform it into a torch tensor
-
 
     # loader for evaluation, keep separate as transformer for train can be different
     eval_transformer = transforms.Compose([
-        transforms.Resize((cfg.IMAGE.TARGET_HEIGHT,cfg.IMAGE.TARGET_WIDTH)),
+        transforms.Resize((cfg.IMAGE.TARGET_HEIGHT, cfg.IMAGE.TARGET_WIDTH)),
         transforms.ToTensor()])  # transform it into a torch tensor
 
     return {
         'train_transformer': train_transformer,
         'eval_transformer': eval_transformer,
     }
-
