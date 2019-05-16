31c4fb38d4443108233b8d37f3ad95ee5c9b5db8
surfertas <tasuku@gmail.com>
2019-05-17 00:04:35 +0900

    remove unused

--- projects/amr_tf_controller/check_devices.py
+++
@@ -1,7 +0,0 @@
-# Checks what devices are available.
-# Quick check to see if GPU is available.
-import tensorflow as tf
-from tensorflow.python.client import device_lib
-
-if __name__=='__main__':
-    device_lib.list_local_devices()
