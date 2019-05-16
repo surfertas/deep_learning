c84aaef4071ce6cac0796ad9573f2346bfa2a909
surfertas <tasuku@gmail.com>
2019-05-16 23:55:59 +0900

    clean up

--- projects/pilotnet_controller/startup.sh
+++
@@ -1,6 +0,0 @@
-source activate amr_py
-echo "Environment activated."
-export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/sa_key.json
-echo "Set google application credentials."
-echo "Start up complete."
-
